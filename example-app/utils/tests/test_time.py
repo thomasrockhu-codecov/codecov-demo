9073e812a8916039fe36ac7a33b0ce695f2eb49d
Tom Hu <tomhu1096+gpg@gmail.com>
2022-01-31 20:29:41 -0500

    Kill it all

--- example-app/utils/tests/test_time.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-from ..time import format_time
-
-def test_time():
-    current_time = datetime.now()
-    assert(format_time(current_time) == datetime.strftime(current_time, '%Y-%m-%d   %H:%M:%S'))
--- example-app/utils/time.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-
-def format_time(time):
-    return datetime.strftime(time, '%Y-%m-%d   %H:%M:%S')
